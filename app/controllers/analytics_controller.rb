910c204550b6d0bf8efffca3dfc467223387dea4
Luiz Picolo <luizpicolo@gmail.com>
2015-09-21 01:11:01 -0400

    criado lista para subcategorias mais acessadas

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -7,6 +7,12 @@ class AnalyticsController < ApplicationController
                                .group(:"product_categories.name")
                                .select(:product_category_id)
                                .order("count_product_category_id desc").count
+
+   @access_subcategory = Analytic.joins(:product_subcategory)
+                                 .where("local = ?", "subcategory")
+                                 .group(:"product_subcategories.name")
+                                 .select(:product_subcategory_id)
+                                 .order("count_product_subcategory_id desc").count
     render layout: false
   end
 end
