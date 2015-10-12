5ce6c2c8470b5f536f2c49c94012db80ca430aad
Luiz Picolo <luizpicolo@gmail.comm>
2015-10-12 13:02:14 -0400

    Adicionado filtro pora dias

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -3,28 +3,33 @@ class AnalyticsController < ApplicationController
 
   def index
     @access_category = Analytic.joins(:product_category)
+                               .where(:created_at => (params[:dateinit].to_date)..(params[:datefini].to_date))
                                .where("local = ?", "category")
                                .group(:"product_categories.name")
                                .select(:product_category_id)
                                .order("count_product_category_id desc").count
 
    @access_subcategory = Analytic.joins(:product_subcategory)
+                                 .where(:created_at => (params[:dateinit].to_date)..(params[:datefini].to_date))
                                  .where("local = ?", "subcategory")
                                  .group(:"product_subcategories.name")
                                  .select(:product_subcategory_id)
                                  .order("count_product_subcategory_id desc").limit(10).count
 
    @access_product = Analytic.joins(:product)
+                             .where(:created_at => (params[:dateinit].to_date)..(params[:datefini].to_date))
                              .where("local = ?", "product")
                              .group(:"products.name")
                              .select(:product_id)
                              .order("count_product_id desc").limit(10).count
 
    @pages = Analytic.joins(:company)
+                    .where(:created_at => (params[:dateinit].to_date)..(params[:datefini].to_date))
                     .where("local != ?", "home").where("local != ?", "product").where("local != ?", "subcategory").where("local != ?", "category")
                     .group(:"companies.slug")
                     .select(:company_id)
                     .order("count_company_id desc").count
+
     render layout: false
   end
 end
