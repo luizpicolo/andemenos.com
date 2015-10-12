b9e8a8f609d77a677331a932cbb475d150420c0a
Luiz Picolo <luizpicolo@gmail.comm>
2015-10-12 17:56:16 -0400

    Fixed relatório de acessos por dia

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -7,38 +7,39 @@ class AnalyticsController < ApplicationController
 
   def show
     @access_category = Analytic.joins(:product_category)
-                               .where(:created_at => (params[:dateinit].to_date)..(params[:datefini].to_date))
-                               .where("local = ?", "category")
-                               .group(:"product_categories.name")
-                               .select(:product_category_id)
-                               .order("count_product_category_id desc").count
+                       .where(:created_at => (params[:dateinit].to_date)..(params[:datefini].to_date))
+                       .where("local = ?", "category")
+                       .group(:"product_categories.name")
+                       .select(:product_category_id)
+                       .order("count_product_category_id desc").count
 
    @access_subcategory = Analytic.joins(:product_subcategory)
-                                 .where(:created_at => (params[:dateinit].to_date)..(params[:datefini].to_date))
-                                 .where("local = ?", "subcategory")
-                                 .group(:"product_subcategories.name")
-                                 .select(:product_subcategory_id)
-                                 .order("count_product_subcategory_id desc").limit(10).count
+                         .where(:created_at => (params[:dateinit].to_date)..(params[:datefini].to_date))
+                         .where("local = ?", "subcategory")
+                         .group(:"product_subcategories.name")
+                         .select(:product_subcategory_id)
+                         .order("count_product_subcategory_id desc").limit(10).count
 
    @access_product = Analytic.joins(:product)
-                             .where(:created_at => (params[:dateinit].to_date)..(params[:datefini].to_date))
-                             .where("local = ?", "product")
-                             .group(:"products.name")
-                             .select(:product_id)
-                             .order("count_product_id desc").limit(10).count
+                     .where(:created_at => (params[:dateinit].to_date)..(params[:datefini].to_date))
+                     .where("local = ?", "product")
+                     .group(:"products.name")
+                     .select(:product_id)
+                     .order("count_product_id desc").limit(10).count
 
    @pages = Analytic.joins(:company)
-                    .where(:created_at => (params[:dateinit].to_date)..(params[:datefini].to_date))
-                    .where("local != ?", "home").where("local != ?", "product").where("local != ?", "subcategory").where("local != ?", "category")
-                    .group(:"companies.slug")
-                    .select(:company_id)
-                    .order("count_company_id desc").count
+            .where(:created_at => (params[:dateinit].to_date)..(params[:datefini].to_date))
+            .where("local != ?", "home").where("local != ?", "product").where("local != ?", "subcategory").where("local != ?", "category")
+            .group(:"companies.slug")
+            .select(:company_id)
+            .order("count_company_id desc").count
 
-    @access_category_days = Analytic
+    @access_category_days = Analytic.joins(:product_category)
                             .where(:created_at => (params[:dateinit].to_date)..(params[:datefini].to_date))
+                            .where("local = ?", "home")
                             .group_by_day(:'analytics.created_at')
-                            .select(:'analytics.created_at')
-                            .order("count_analytics_created_at desc").count
+                            .select(:product_category_id)
+                            .order("count_product_category_id desc").count
 
 
     render layout: false
